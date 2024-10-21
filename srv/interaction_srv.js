83676b75695bbc7e9d8b570b6bbfe3299a23179f
Liandro Cruz <liandrocruz@kpmg.com>
2024-10-21 22:15:26 +0000

    verificaçoes

--- srv/interaction_srv.js
+++ srv/interaction_srv.js
@@ -5,23 +5,30 @@ module.exports = async (srv) => {
     const { Approvers, Sales, SalesItems, Material } = db.entities;
 
     srv.on('create_salesorder', async (req) => {
-        const { email_vendedor, email_comprador, items } = req.data;
+        const { email_vendor, email_comprador, items } = req.data;
 
-        const vendorRow = await SELECT.one.from(Approvers).where({ vendor: email_vendedor });
+        if (!email_vendor || !email_comprador || !items || items.length === 0) {
+            return 'Dados de entrada inválidos.';
+          }
+
+        // Obter o aprovador
+        const approver = await SELECT.from(Approvers).where({ vendor: email_vendor });
+        if (!approver || approver.length === 0) {
+            return `Aprovador com email ${email_vendor} não encontrado.`;
+        }
+
         
-
+        let valor_total = 0;
         if (items && items.length > 0) {
             for (const item of items) {
-
                 const material = await SELECT.one.from(Material).where({ name: item.material });
-
-                if (!material) {
-                    return (`Material ${item.material} não encontrado`);
-                }
-
-                // Verifica se a quantidade pedida é maior que a quantidade disponível em stocksss
+                    if (!material) {
+                        return (`Material ${item.material} não encontrado`);
+                    }
+                    
+                // Verifica se a quantidade pedida é maior que a quantidade disponível em stock
                 if (item.qtd > material.stock) {
-                    return (`Quantidade pedida (${item.qtd}) ultrapasa o stock disponível (${material.stock}) para o material ${material.name}`);
+                    return (`Quantidade pedida (${item.qtd}) ultrapasa o stock disponível (${material.stock}) para o material `);
                 }
 
                 const item_total = item.price_unit * item.qtd;
@@ -31,15 +38,16 @@ module.exports = async (srv) => {
         }
         const newSale = {
             comprador: email_comprador,
-            vendor: vendorRow,
+            vendor: approver,
             valor_total: valor_total,
             status: "Pendente",
-            valor_iva: valor_total + (valor_total * 0.23),
+            valor_iva: 1.23
+
         };
         const [result] = await INSERT(Sales).into(newSale);
 
 
-        let valor_total = 0;
+       
         let itemCounter = 1;
         for (const item of items) {
             const material = await SELECT.one.from(Material).where({ name: item.material });
